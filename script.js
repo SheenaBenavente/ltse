27f05c062bd7f0add5fb63eb3b8f2d5b8175a222
sheena benavente <13785060+SheenaBenavente@users.noreply.github>
2023-02-14 10:15:24 -0500

    added menu close and open function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const mobileMenuIcon = document.querySelector('#mobile-menu-icon');
 const mobileMenuList = document.querySelector('#mobile-menu-list');
 const mobileExitMenuIcon = document.querySelector('#mobile-exit-menu-icon');
 
+// Clicking on the hamburger menu drops down the main menu
 mobileMenuIcon.addEventListener('click', function() {
   // 1. Unhide menu links/list
   mobileMenuList.classList.remove('hide');
@@ -13,4 +14,14 @@ mobileMenuIcon.addEventListener('click', function() {
   mobileExitMenuIcon.classList.remove('hide');
 });
 
-// TODO: Clicking on the exit menu icon should restore things back to default
+// Clicking on the exit menu restores the menu back to default
+mobileExitMenuIcon.addEventListener('click', function() {
+  // 1. Hide menu links/list
+  mobileMenuList.classList.add('hide');
+  // 2. Show hamburger menu icon
+  mobileMenuIcon.classList.remove('hide');
+  // 3. Hide the exit menu icon
+  mobileExitMenuIcon.classList.add('hide');
+});
+
+// TODO: Click on the Topics menu hides the topics
